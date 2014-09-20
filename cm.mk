a35d1e9f2a4070eaa8c1b1ba7bd90b0ef23f6b7c
sssemil <suleymanovemil8@gmail.com>
2014-09-20 14:44:36 +0500

    dogo: update fingerprint from 10.5.1.A.0.283
    Change-Id: Idd6ee287446a01286120967de00ffb4939135ac2

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5503 BUILD_FINGERPRINT=Sony/C5503_1270-6697/C5503:4.2.2/10.3.A.0.423/WP5_rg:user/release-keys PRIVATE_BUILD_DESC="C5503-user 4.2.2 10.3.A.0.423 WP5_rg test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5503 BUILD_FINGERPRINT=Sony/C5503/C5503:4.4.4/10.5.1.A.0.283/5P53rQ:user/release-keys PRIVATE_BUILD_DESC="C5503-user 4.4.4 10.5.1.A.0.283 5P53rQ test-keys"
 
 PRODUCT_NAME := cm_dogo
 PRODUCT_DEVICE := dogo
