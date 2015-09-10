d6773c3d54e0a3909f007fb77b6852910317f5f4
Georg Veichtlbauer <georg.veichtlbauer@gmail.com>
2015-09-10 12:41:38 +0200

    update build fingerprint
    Change-Id: I793238f106ec739b49ec2e9e8e2314045c6ad437

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5503 BUILD_FINGERPRINT=Sony/C5503/C5503:5.0.2/10.6.A.0.454/266452109:user/release-keys PRIVATE_BUILD_DESC="C5503-user 5.0.2 10.6.A.0.454 266452109 test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5503 BUILD_FINGERPRINT=Sony/C5503/C5503:5.1.1/10.7.A.0.222/1314554057:user/release-keys PRIVATE_BUILD_DESC="C5503-user 5.1.1 10.7.A.0.222 1314554057 test-keys"
 
 PRODUCT_NAME := cm_dogo
 PRODUCT_DEVICE := dogo
