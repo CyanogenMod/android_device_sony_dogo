f6e5bb4f25a064a2967d762ada22d429385318bd
Olivier Karasangabo <olivier@cyanogenmod.org>
2016-03-31 00:01:37 +0200

    dogo: Deprecate common Sony resources
    Change-Id: I6c442d04403fa34ccff04e561c42e89bb289c9b7

--- full_dogo.mk
+++ full_dogo.mk
@@ -28,9 +28,6 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Common Sony Resources
-$(call inherit-product, device/sony/common/resources.mk)
-
 # Inherit from dogo device
 $(call inherit-product, device/sony/dogo/dogo.mk)
 
