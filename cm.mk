fe18a08a2a5798d40e1e60890fa14b90aacb8809
AdrianDC <radian.dc@gmail.com>
2016-05-30 16:19:16 +0300

    dogo: Restructure the device tree into fragments
     * Explode all configurations and makefiles
        into feature-related files automatically imported
    
     * Based upon the fragmented structure introduced
        in android_device_nextbit_ether by Steve Kondik
    
     * Reworked for fusion3 by Chippa_a
    
    Change-Id: I952bc49056108eb912bc7b0b7366ba2122632611
    Signed-off-by: AdrianDC <radian.dc@gmail.com>
    Signed-off-by: Chippa-a <vusal1372@gmail.com>

--- cm.mk
+++ cm.mk
@@ -1,12 +1,37 @@
-$(call inherit-product, device/sony/dogo/full_dogo.mk)
+#
+# Copyright (C) 2013-2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Device display
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-# Inherit CM common Phone stuff.
+# Inherit common CM stuff
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5503 BUILD_FINGERPRINT=Sony/C5503/C5503:5.1.1/10.7.A.0.222/1314554057:user/release-keys PRIVATE_BUILD_DESC="C5503-user 5.1.1 10.7.A.0.222 1314554057 test-keys"
+# Inherit device configurations
+$(call inherit-product, device/sony/dogo/device.mk)
 
-PRODUCT_NAME := cm_dogo
+# Device identifications
 PRODUCT_DEVICE := dogo
+PRODUCT_NAME := cm_dogo
+PRODUCT_BRAND := Sony
+PRODUCT_MANUFACTURER := Sony
+PRODUCT_MODEL := Xperia ZR
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5503 BUILD_FINGERPRINT=Sony/C5503/C5503:5.1.1/10.7.A.0.222/1314554057:user/release-keys PRIVATE_BUILD_DESC="C5503-user 5.1.1 10.7.A.0.222 1314554057 test-keys"
